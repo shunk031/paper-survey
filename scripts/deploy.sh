044f849f225d45ed0eab78e040fb6c0e4419b19d
Shunsuke KITADA <septemtrio.ager@gmail.com>
2018-02-16 16:24:23 +0900

    change script path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-    ./compile.sh
+    ./scripts/compile.sh
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify

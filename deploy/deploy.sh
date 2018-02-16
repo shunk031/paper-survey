2392a205c6613df74c4f2ec2bf258f11062572b3
Shunsuke KITADA <septemtrio.ager@gmail.com>
2018-02-17 01:29:29 +0900

    update

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -22,8 +22,6 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
     exit 0
 fi
 
-ls
-
 # Save some useful information
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
@@ -31,9 +29,13 @@ SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
+git clone $REPO _site
+cd _site
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+cd ..
+
+# Clean out existing contents
+rm -rf out/**/* || exit 0
 
 # Run our compile script
 doCompile
@@ -41,8 +43,6 @@ doCompile
 # Now let's go have some fun with the cloned repo
 git config user.name "Shunsuke KITADA"
 git config user.email "septemtrio.ager@gmail.com"
-
-ls
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 if git diff --quiet; then

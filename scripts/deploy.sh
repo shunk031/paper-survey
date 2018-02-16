77eec7557627d53baaa1a8c5c62bdb719365e90b
Shunsuke KITADA <septemtrio.ager@gmail.com>
2018-02-16 16:07:25 +0900

    add deploy scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,30 +1,60 @@
 #!/bin/bash
 
-# skip if build is triggered by pull request
-if [ $TRAVIS_PULL_REQUEST == "true" ]; then
-  echo "this is PR, exiting"
-  exit 0
-fi
+echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy.key\n" >> ~/.ssh/config
 
 set -e # Exit with nonzero exit code if anything fails
 
-echo -e "Host github.com\n\tStrictHostKeyChecking no\nIdentityFile ~/.ssh/deploy.key\n" >> ~/.ssh/config
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+
+function doCompile {
+    ./compile.sh
+}
+
+# Pull requests and commits to other branches shouldn't try to deploy, just build to verify
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    doCompile
+    exit 0
+fi
+
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+# Clone the existing gh-pages for this repo into out/
+# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
+git clone $REPO out
+cd out
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+cd ..
+
+# Clean out existing contents
+rm -rf out/**/* || exit 0
+
+# Run our compile script
+doCompile
+
+# Now let's go have some fun with the cloned repo
+cd out
+git config user.name "Shunsuke KITADA"
+git config user.email "septemtrio.ager@gmail.com"
+
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+if git diff --quiet; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
+
+# Commit the "changes", i.e. the new version.
+# The delta will show diffs between new and old versions.
+git add -A .
+git commit -m "[Travis CI] Deploy to GitHub Pages: ${SHA}"
 
 openssl aes-256-cbc -K $encrypted_0c5788daacb5_key -iv $encrypted_0c5788daacb5_iv -in deploy_paper-survey_key.enc -out ~\/.ssh/deploy_paper-survey_key -d
+chmod 600 ~\/.ssh/deploy_paper-survey_key
+# eval `ssh-agent -s`
+# ssh-add deploy_paper-survey_key
 
-# cleanup "_site"
-rm -rf _site
-mkdir _site
-
-
-git clone git@github.com:shunk031/paper-survey -b gh-pages _site
-
-# build with Jekyll into "_site"
-bundle exec jekyll build
-
-cd _site
-git config user.email "septemtrio.ager@gmail.com"
-git config user.name "Shunsuke KITADA"
-git add --all
-git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
-git push --force origin gh-pages
+git push $SSH_REPO $TARGET_BRANCH

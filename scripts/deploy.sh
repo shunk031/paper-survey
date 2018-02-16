270868dc1134b765b3125a5a3a2b6732c78aef83
Shunsuke KITADA <septemtrio.ager@gmail.com>
2018-02-16 22:30:13 +0900

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-    .out/scripts/compile.sh
+    ./scripts/compile.sh
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify

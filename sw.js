3cc8eeceec63d44b85fd661985c91d9dcf535566
Shunsuke KITADA <septemtrio.ager@gmail.com>
2018-05-07 05:59:41 +0000

    [Travis CI] Deploy to GitHub Pages: 387daaae03f5d5d59ab7aace74e4ef195d2ed156

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '20180506150949';
+var CACHE_NAME = '20180507055920';
 
 self.addEventListener('install',event => {
   event.waitUntil(caches.open(CACHE_NAME)
@@ -41,10 +41,11 @@ self.addEventListener('install',event => {
     '/paper-survey/summary/25/',
     '/paper-survey/summary/26/',
     '/paper-survey/summary/27/',
+    '/paper-survey/summary/28/',
     
     
     
-    '/paper-survey/summary/nlp/Utilizing-Visual-Forms-of-Japanese-Characters-for-Neural-Review-Classification','/paper-survey/summary/cv/Realistic-Evaluation-of-Semi-Supervised-Learning-Algorithms','/paper-survey/summary/nlp/Word-Embedding-Perturbation-for-Sentence-Classification','/paper-survey/summary/nlp/Learning-to-Compute-Word-Embeddings-On-the-Fly','/paper-survey/summary/nlp/Glyph-aware-Embedding-of-Chinese-Characters','/paper-survey/summary/nlp/A-New-Method-of-Region-Embedding-for-Text-Classification','/paper-survey/summary/others/Variable-Importance-Using-Decision-Trees','/paper-survey/summary/others/Unsupervised-Learning-of-Disentangled-and-Interpretable-Representations-from-Sequential-Data','/paper-survey/summary/cv/Sparse-Embedded-k-Means-Clustering',
+    '/paper-survey/summary/nlp/Hierarchical-Attention-Networks-for-Document-Classification','/paper-survey/summary/nlp/Utilizing-Visual-Forms-of-Japanese-Characters-for-Neural-Review-Classification','/paper-survey/summary/cv/Realistic-Evaluation-of-Semi-Supervised-Learning-Algorithms','/paper-survey/summary/nlp/Word-Embedding-Perturbation-for-Sentence-Classification','/paper-survey/summary/nlp/Learning-to-Compute-Word-Embeddings-On-the-Fly','/paper-survey/summary/nlp/Glyph-aware-Embedding-of-Chinese-Characters','/paper-survey/summary/nlp/A-New-Method-of-Region-Embedding-for-Text-Classification','/paper-survey/summary/others/Variable-Importance-Using-Decision-Trees','/paper-survey/summary/others/Unsupervised-Learning-of-Disentangled-and-Interpretable-Representations-from-Sequential-Data',
   ]))
 );
 });

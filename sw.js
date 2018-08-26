568d1561dcd2eca6369da020533b329a2bc5f974
Shunsuke KITADA <septemtrio.ager@gmail.com>
2018-08-26 09:15:05 +0000

    [Travis CI] Deploy to GitHub Pages: 1c4ef18e66b1f7b63ed6389ea6860b7ac5dc0bb3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '20180826091319';
+var CACHE_NAME = '20180826091436';
 
 self.addEventListener('install',event => {
   event.waitUntil(caches.open(CACHE_NAME)
@@ -43,10 +43,11 @@ self.addEventListener('install',event => {
     '/paper-survey/summary/27/',
     '/paper-survey/summary/28/',
     '/paper-survey/summary/29/',
+    '/paper-survey/summary/30/',
     
     
     
-    '/paper-survey/summary/nlp/Joint-Embedding-of-Words-and-Labels-for-Text-Classification','/paper-survey/summary/cv/Learn-to-Pay-Attention','/paper-survey/summary/others/Skin-Lesion-Analysis-Toward-Melanoma-Detection-A-Challenge-at-the-2017-International-Symposium-on-Biomedical-Imaging-ISBI-Hosted-by-the-International-Skin-Imaging-Collaboration-ISIC','/paper-survey/summary/others/RECOD-Titans-at-ISIC-Challenge-2017','/paper-survey/summary/nip/Contextual-Augmentation-Data-Augmentation-by-Words-with-Paradigmatic-Relations','/paper-survey/summary/nlp/Hierarchical-Attention-Networks-for-Document-Classification','/paper-survey/summary/nlp/Utilizing-Visual-Forms-of-Japanese-Characters-for-Neural-Review-Classification','/paper-survey/summary/cv/Realistic-Evaluation-of-Semi-Supervised-Learning-Algorithms','/paper-survey/summary/nlp/Word-Embedding-Perturbation-for-Sentence-Classification',
+    '/paper-survey/summary/cv/Semi-supervised-deep-learning-by-metric-embedding','/paper-survey/summary/nlp/Joint-Embedding-of-Words-and-Labels-for-Text-Classification','/paper-survey/summary/cv/Learn-to-Pay-Attention','/paper-survey/summary/others/Skin-Lesion-Analysis-Toward-Melanoma-Detection-A-Challenge-at-the-2017-International-Symposium-on-Biomedical-Imaging-ISBI-Hosted-by-the-International-Skin-Imaging-Collaboration-ISIC','/paper-survey/summary/others/RECOD-Titans-at-ISIC-Challenge-2017','/paper-survey/summary/nip/Contextual-Augmentation-Data-Augmentation-by-Words-with-Paradigmatic-Relations','/paper-survey/summary/nlp/Hierarchical-Attention-Networks-for-Document-Classification','/paper-survey/summary/nlp/Utilizing-Visual-Forms-of-Japanese-Characters-for-Neural-Review-Classification','/paper-survey/summary/cv/Realistic-Evaluation-of-Semi-Supervised-Learning-Algorithms',
   ]))
 );
 });

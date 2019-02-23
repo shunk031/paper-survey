d58b49a9c69bcb25dfb57884a7f728744e40a938
Shunsuke KITADA <septemtrio.ager@gmail.com>
2019-02-23 05:42:57 +0000

    [Travis CI] Deploy to GitHub Pages: c3ed6b8de27e8785020e77690e5ac2be6bdd1b22

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '20190223053249';
+var CACHE_NAME = '20190223054236';
 
 self.addEventListener('install',event => {
   event.waitUntil(caches.open(CACHE_NAME)
@@ -45,10 +45,11 @@ self.addEventListener('install',event => {
     '/paper-survey/summary/28/',
     '/paper-survey/summary/29/',
     '/paper-survey/summary/30/',
+    '/paper-survey/summary/31/',
     
     
     
-    '/paper-survey/summary/others/Field-aware-Probabilistic-Embedding-Neural-Network-for-CTR-Prediction','/paper-survey/summary/nlp/Context-Dependent-Sentiment_analysis-in-User-Genereted-Videos','/paper-survey/summary/cv/Semi-supervised-deep-learning-by-metric-embedding','/paper-survey/summary/nlp/Joint-Embedding-of-Words-and-Labels-for-Text-Classification','/paper-survey/summary/cv/Learn-to-Pay-Attention','/paper-survey/summary/others/Skin-Lesion-Analysis-Toward-Melanoma-Detection-A-Challenge-at-the-2017-International-Symposium-on-Biomedical-Imaging-ISBI-Hosted-by-the-International-Skin-Imaging-Collaboration-ISIC','/paper-survey/summary/others/RECOD-Titans-at-ISIC-Challenge-2017','/paper-survey/summary/nip/Contextual-Augmentation-Data-Augmentation-by-Words-with-Paradigmatic-Relations','/paper-survey/summary/nlp/Hierarchical-Attention-Networks-for-Document-Classification',
+    '/paper-survey/summary/cv/SC-FEGAN-Face-Editing-Generative-Adversarial-Networks-with-Users-Sketch-and-Color','/paper-survey/summary/others/Field-aware-Probabilistic-Embedding-Neural-Network-for-CTR-Prediction','/paper-survey/summary/nlp/Context-Dependent-Sentiment_analysis-in-User-Genereted-Videos','/paper-survey/summary/cv/Semi-supervised-deep-learning-by-metric-embedding','/paper-survey/summary/nlp/Joint-Embedding-of-Words-and-Labels-for-Text-Classification','/paper-survey/summary/cv/Learn-to-Pay-Attention','/paper-survey/summary/others/Skin-Lesion-Analysis-Toward-Melanoma-Detection-A-Challenge-at-the-2017-International-Symposium-on-Biomedical-Imaging-ISBI-Hosted-by-the-International-Skin-Imaging-Collaboration-ISIC','/paper-survey/summary/others/RECOD-Titans-at-ISIC-Challenge-2017','/paper-survey/summary/nip/Contextual-Augmentation-Data-Augmentation-by-Words-with-Paradigmatic-Relations',
   ]))
 );
 });

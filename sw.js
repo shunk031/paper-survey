a72b2236f45c446ed378006e099e29f0451bcc1d
Shunsuke KITADA <septemtrio.ager@gmail.com>
2019-08-15 11:55:01 +0000

    [Travis CI] Deploy to GitHub Pages: bc97600be11dd4a365e32150eb37a1a780cb8453

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = '20190727153230';
+var CACHE_NAME = '20190815115438';
 
 self.addEventListener('install',event => {
   event.waitUntil(caches.open(CACHE_NAME)
@@ -52,7 +52,7 @@ self.addEventListener('install',event => {
     
     
     
-    '/paper-survey/summary/nlp/DropAttention-A-Regularization-Method-for-Fully-Connected-Self-Attention-Networks','/paper-survey/summary/nlp/From-Small-scale-to-Large-scale-Text-Classification','/paper-survey/summary/others/Content-Based-Citation-Recommendation','/paper-survey/summary/cv/Class-Balanced-Loss-Based-on-Effective-Number-of-Samples','/paper-survey/summary/others/Attention-Convolutional-Neural-Network-for-Advertiser-level-Click-through-Rate-Forecasting','/paper-survey/summary/others/Beyond-News-Contents-The-Role-of-Social-Context-for-Fake-News-Detection','/paper-survey/summary/nlp/How-Large-Vocabulary-Does-Text-Classification-Need-A-Variational-Approach-to-Vocabulary-Selection','/paper-survey/summary/nlp/Attentional-Encoder-Network-for-Targeted-Sentiment-Classification','/paper-survey/summary/cv/Tell-Me-Where-to-Look-Guided-Attention-Inference-Network',
+    '/paper-survey/summary/others/Putting-Fairness-Principles-into-Practice-Challenges-Metrics-and-Improvements','/paper-survey/summary/nlp/DropAttention-A-Regularization-Method-for-Fully-Connected-Self-Attention-Networks','/paper-survey/summary/nlp/From-Small-scale-to-Large-scale-Text-Classification','/paper-survey/summary/others/Content-Based-Citation-Recommendation','/paper-survey/summary/cv/Class-Balanced-Loss-Based-on-Effective-Number-of-Samples','/paper-survey/summary/others/Attention-Convolutional-Neural-Network-for-Advertiser-level-Click-through-Rate-Forecasting','/paper-survey/summary/others/Beyond-News-Contents-The-Role-of-Social-Context-for-Fake-News-Detection','/paper-survey/summary/nlp/How-Large-Vocabulary-Does-Text-Classification-Need-A-Variational-Approach-to-Vocabulary-Selection','/paper-survey/summary/nlp/Attentional-Encoder-Network-for-Targeted-Sentiment-Classification',
   ]))
 );
 });
